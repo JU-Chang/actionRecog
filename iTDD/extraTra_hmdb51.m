1cf7182d212a3abf3b712cd54beed6c8be7f88fe
JU-Chang <zhang-yinzhu@qq.com>
2018-01-09 16:30:19 +0800

    extra tra of hmdb51

---
+++ iTDD/extraTra_hmdb51.m
@@ -0,0 +1,67 @@
+% iDT extraction of ucf11.
+% @author=zz
+
+function extraTra_hmdb51()
+
+
+% path1 = getenv('LD_LIBRARY_PATH');             %获得系统路径的字符串
+% path1 = ['/usr/lib:' path1];   %字符串中加入自己要的路径
+% % path1 = [path1 '::/usr/local/ffmpeg/bin  /home/chang/anaconda2/bin/']   %字符串中加入自己要的路径
+% setenv('LD_LIBRARY_PATH', path1);                %设置系统路径
+% %输出以查看之
+% !echo $PATH                              
+
+data_dir = '/data/HMDB51/hmdb51_org/unrar';
+tra_dir = '/data/HMDB51/tra_dir';
+
+if ~exist(tra_dir,'dir')
+    mkdir(tra_dir);
+elseif length(dir(tra_dir)) > 2                 % check dir validation
+    error(['trajecotry:"',tra_dir,'" already exist!']); 
+end
+
+if ~exist(data_dir,'dir')              % check dir validation
+    error(['data dir:"',data_dir,'" not exist!']); 
+end
+
+function folderlist = getFolderList(ddir)
+    folderlist = dir(ddir);
+    folderlist = {folderlist(:).name};
+    folderlist = setdiff(folderlist,{'.','..'});
+end
+
+
+classlist = getFolderList(data_dir);
+for i = 1:length(classlist)
+    video_dir = fullfile(data_dir,classlist{i});
+    vtra_dir = fullfile(tra_dir,classlist{i});
+    if ~exist(vtra_dir,'dir')
+        mkdir(vtra_dir);
+        display(['processing',vtra_dir]);
+    else
+        error('target file exist!')
+    end
+
+    videolist = getFolderList(fullfile(video_dir,'*.avi'));
+   
+    for k = 1:length(videolist)
+        videofile = fullfile(video_dir,videolist{k});
+        vtra_file = fullfile(vtra_dir,videolist{k});
+        vtra_file = [vtra_file(1:end-4),'.bin'];
+        %display('Extract improved trajectories...');
+        system(['LD_LIBRARY_PATH=/usr/lib /home/chang/code/actionRecog/iTDD/DenseTrackStab -f ',videofile,' -o ',vtra_file]);
+    end
+end
+
+display('done');
+
+
+
+
+% iDT extraction
+% display('Extract improved trajectories...');
+% function  extract_fv(index, power, layer, tag ,dim1, norm)
+% system(['./DenseTrackStab -f ',vid_name,' -o ',vid_name(1:end-4),'.bin']);
+
+
+end
